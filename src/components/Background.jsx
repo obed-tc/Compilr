c1e482c9092450939dba0f6cd69c51482e8ef3be
obed-tc <castroobed53@gmail.com>
2024-11-23 15:43:47 -0400

    feat: Add background and assets

---
+++ src/components/Background.jsx
@@ -0,0 +1,10 @@
+import "../styles/style.css";
+function Background() {
+  return (
+    <div className="absolute top-0 ">
+      <div className="hero"> </div>
+    </div>
+  );
+}
+
+export default Background;
