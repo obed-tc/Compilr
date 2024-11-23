1d64b9dc213df01ee72daf85495bd6bada3b8c30
obed-tc <castroobed53@gmail.com>
2024-11-23 15:46:06 -0400

    feat: Integrate components in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Background from "./components/Background";
 import ExecuteJS from "./components/ExecuteJS";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="bg-[#171717] min-h-screen text-white py-5">
-      <h1 className="text-center text-xl">Compilador Javascript en linea</h1>
-      <ExecuteJS />
+    <div className="bg-[#000] min-h-screen text-white   relative">
+      <Background />
+      <div className="relative z-20 flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">
+          <ExecuteJS />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
